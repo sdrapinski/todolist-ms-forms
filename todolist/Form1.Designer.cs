895e47701e2a7ade37743a4811a3252e4ff747ca
Student <Student@DESKTOP-KGK4MVM>
2024-04-05 14:39:37 +0200

    Add project files.

---
+++ todolist/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace todolist
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.titleColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.dueDateColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.newButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.descriptionBox = new System.Windows.Forms.TextBox();
+            this.titleInput = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.entriesSource = new System.Windows.Forms.BindingSource(this.components);
+            this.buttonfromfile = new System.Windows.Forms.Button();
+            this.buttonToFile = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.entriesSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.panel1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.buttonToFile);
+            this.splitContainer1.Panel2.Controls.Add(this.buttonfromfile);
+            this.splitContainer1.Panel2.Controls.Add(this.newButton);
+            this.splitContainer1.Panel2.Controls.Add(this.deleteButton);
+            this.splitContainer1.Panel2.Controls.Add(this.dateTimePicker1);
+            this.splitContainer1.Panel2.Controls.Add(this.descriptionBox);
+            this.splitContainer1.Panel2.Controls.Add(this.titleInput);
+            this.splitContainer1.Panel2.Controls.Add(this.label3);
+            this.splitContainer1.Panel2.Controls.Add(this.label2);
+            this.splitContainer1.Panel2.Controls.Add(this.label1);
+            this.splitContainer1.Size = new System.Drawing.Size(453, 450);
+            this.splitContainer1.SplitterDistance = 266;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.listView1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(453, 266);
+            this.panel1.TabIndex = 0;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.titleColumn,
+            this.dueDateColumn});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(453, 266);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // titleColumn
+            // 
+            this.titleColumn.Text = "Tytul";
+            this.titleColumn.Width = 342;
+            // 
+            // dueDateColumn
+            // 
+            this.dueDateColumn.Text = "Do dnia";
+            this.dueDateColumn.Width = 107;
+            // 
+            // newButton
+            // 
+            this.newButton.Location = new System.Drawing.Point(160, 145);
+            this.newButton.Name = "newButton";
+            this.newButton.Size = new System.Drawing.Size(75, 23);
+            this.newButton.TabIndex = 7;
+            this.newButton.Text = "&Nowe";
+            this.newButton.UseVisualStyleBackColor = true;
+            this.newButton.MouseClick += new System.Windows.Forms.MouseEventHandler(this.newButton_MouseClick);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(69, 145);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(75, 23);
+            this.deleteButton.TabIndex = 6;
+            this.deleteButton.Text = "&Usun";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.MouseClick += new System.Windows.Forms.MouseEventHandler(this.deleteButton_MouseClick);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(69, 43);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(384, 20);
+            this.dateTimePicker1.TabIndex = 5;
+            // 
+            // descriptionBox
+            // 
+            this.descriptionBox.AcceptsReturn = true;
+            this.descriptionBox.AcceptsTab = true;
+            this.descriptionBox.Location = new System.Drawing.Point(69, 77);
+            this.descriptionBox.Multiline = true;
+            this.descriptionBox.Name = "descriptionBox";
+            this.descriptionBox.Size = new System.Drawing.Size(381, 62);
+            this.descriptionBox.TabIndex = 4;
+            // 
+            // titleInput
+            // 
+            this.titleInput.Location = new System.Drawing.Point(69, 13);
+            this.titleInput.Name = "titleInput";
+            this.titleInput.Size = new System.Drawing.Size(381, 20);
+            this.titleInput.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Opis";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Do Dnia";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "&Tytuł";
+            // 
+            // entriesSource
+            // 
+            this.entriesSource.DataSource = typeof(todolist.ToDoEntry);
+            this.entriesSource.ListChanged += new System.ComponentModel.ListChangedEventHandler(this.entriesSource_ListChanged);
+            // 
+            // buttonfromfile
+            // 
+            this.buttonfromfile.Location = new System.Drawing.Point(254, 144);
+            this.buttonfromfile.Name = "buttonfromfile";
+            this.buttonfromfile.Size = new System.Drawing.Size(93, 23);
+            this.buttonfromfile.TabIndex = 8;
+            this.buttonfromfile.Text = "Pobierz Z pliku";
+            this.buttonfromfile.UseVisualStyleBackColor = true;
+            this.buttonfromfile.Click += new System.EventHandler(this.buttonfromfile_Click);
+            // 
+            // buttonToFile
+            // 
+            this.buttonToFile.Location = new System.Drawing.Point(353, 145);
+            this.buttonToFile.Name = "buttonToFile";
+            this.buttonToFile.Size = new System.Drawing.Size(96, 23);
+            this.buttonToFile.TabIndex = 9;
+            this.buttonToFile.Text = "Zapisz Do pliku";
+            this.buttonToFile.UseVisualStyleBackColor = true;
+            this.buttonToFile.Click += new System.EventHandler(this.buttonToFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.entriesSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingSource entriesSource;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader titleColumn;
+        private System.Windows.Forms.ColumnHeader dueDateColumn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox descriptionBox;
+        private System.Windows.Forms.TextBox titleInput;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button newButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button buttonfromfile;
+        private System.Windows.Forms.Button buttonToFile;
+    }
+}
+
---
+++ todolist/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.Json;
+using System.IO;
+
+namespace todolist
+{
+    public partial class Form1 : Form
+    {
+        private BindingList<ToDoEntry> entries = new BindingList<ToDoEntry>();
+        public Form1()
+        {
+            InitializeComponent();
+            titleInput.DataBindings.Add("Text", entriesSource,"Title" ,true, DataSourceUpdateMode.OnPropertyChanged);
+            descriptionBox.DataBindings.Add("Text", entriesSource, "Description", true, DataSourceUpdateMode.OnPropertyChanged);
+            dateTimePicker1.DataBindings.Add("Value", entriesSource, "DueDate", true, DataSourceUpdateMode.OnPropertyChanged);
+           
+            entriesSource.DataSource = entries;
+            createNewItem();
+        }
+
+         private void createNewItem()
+        {
+            ToDoEntry newEntry = (ToDoEntry)entriesSource.AddNew();
+            newEntry.Title = "nowe zadanie";
+            newEntry.Description = "nowy opis";
+            newEntry.DueDate = DateTime.Now;
+            entriesSource.ResetCurrentItem();
+        }
+
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void entriesSource_ListChanged(object sender, ListChangedEventArgs e)
+        {
+            switch(e.ListChangedType)
+            {
+                case ListChangedType.ItemAdded:
+                    MakeListViewItemForNewEntry(e.NewIndex);
+                    break;
+                case ListChangedType.ItemDeleted:
+                    RemoveListViewItem(e.NewIndex);
+                    break;
+                case ListChangedType.ItemChanged:
+                    UpdateListViewItem(e.NewIndex);
+                    break;
+            }
+        }
+
+        private void MakeListViewItemForNewEntry(int newItemIndex)
+        {
+            ListViewItem item = new ListViewItem();
+            item.SubItems.Add("");
+            listView1.Items.Insert(newItemIndex,item);
+        }
+
+        private void RemoveListViewItem(int deletedItemIndex)
+        {
+            listView1.Items.RemoveAt(deletedItemIndex);
+        }
+
+        private void UpdateListViewItem(int itemIndex)
+        {
+            ListViewItem listItem = listView1.Items[itemIndex];
+            ToDoEntry entry = entries[itemIndex];
+            listItem.SubItems[0].Text = entry.Title;
+            listItem.SubItems[1].Text = entry.DueDate.ToString();
+        }
+
+        private void deleteButton_MouseClick(object sender, MouseEventArgs e)
+        {
+            if(listView1.SelectedItems.Count >0)
+            {
+                int index = listView1.SelectedIndices[0];
+                entriesSource.RemoveAt(index);
+            }
+         
+        }
+
+        private void newButton_MouseClick(object sender, MouseEventArgs e)
+        {
+            createNewItem();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                int index = listView1.SelectedIndices[0];
+                entriesSource.Position = index;
+            }
+        }
+
+        private void buttonToFile_Click(object sender, EventArgs e)
+        {
+            // export
+            StreamWriter sw = new StreamWriter("todo.json");
+            string jsonString = JsonSerializer.Serialize(entries);
+            sw.WriteLine(jsonString);
+            sw.Close();
+
+        }
+
+        private void buttonfromfile_Click(object sender, EventArgs e)
+        {
+            //import
+            StreamReader sr = new StreamReader("todo.json");
+           string line = sr.ReadLine();
+           entries = JsonSerializer.Deserialize<BindingList<ToDoEntry>>(line);
+            sr.Close();
+
+        }
+    }
+}
